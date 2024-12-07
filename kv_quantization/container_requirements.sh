c353b12b1bcaf2c1f21a52b89ab179d3a8bbad23
Janghyeon-Kim <kkt20@hanyang.ac.kr>
2024-12-07 13:28:39 +0000

    Add kv_quantization

---
+++ kv_quantization/container_requirements.sh
@@ -0,0 +1,4 @@
+pip install torch==2.1.2+cu118 --extra-index-url https://download.pytorch.org/whl/cu118;
+pip install torchaudio==2.1.2+cu118 --extra-index-url https://download.pytorch.org/whl/cu118;
+pip install torchvision==0.16.2+cu118 --extra-index-url https://download.pytorch.org/whl/cu118;
+pip install -r container_requirements.txt;
